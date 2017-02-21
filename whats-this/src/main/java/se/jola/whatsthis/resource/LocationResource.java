5174039bf91cab0510e2860045afe471c9144ce5
Joakim Landström <joakim.landstrom@outlook.com>
2017-02-21 19:39:47 +0100

    front end started

--- whats-this/src/main/java/se/jola/whatsthis/resource/LocationResource.java
+++ whats-this/src/main/java/se/jola/whatsthis/resource/LocationResource.java
@@ -1,21 +1,21 @@
 package se.jola.whatsthis.resource;
 
-import javax.ws.rs.PathParam;
 import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import se.jola.whatsthis.model.LocationRequestBean;
 import se.jola.whatsthis.service.LocationService;
 
-@Component
+@Service
 @Path("locations")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
